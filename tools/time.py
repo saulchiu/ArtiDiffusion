4c88423045dc74cbf0f8ce45d9bc78ccd7b7c688
chengyiqiu1121 <chengyiqiu1121@gmail.com>
2024-04-25 20:12:41 +0800

    pc add beijing time

--- tools/time.py
+++ tools/time.py
@@ -1,15 +1,17 @@
 import time
 from datetime import datetime
+import requests
+import pytz
 
 
 def get_hour():
-    now = datetime.now()
-    current_hour = now.strftime("%H")
-    return current_hour
+    bj_tz = pytz.timezone('Asia/Shanghai')
+    bj_time = datetime.now(bj_tz)
+    return bj_time.hour
 
 
 if __name__ == '__main__':
-    server = 'lv'
+    server = 'lab'
     if server == 'lab':
         while True:
             current_hour = get_hour()
