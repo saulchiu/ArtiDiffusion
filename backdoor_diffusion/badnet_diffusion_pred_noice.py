879a1dbdcc429e8da791559e367b013c87bb8fc1
chengyiqiu1121 <chengyiqiu1121@gmail.com>
2024-05-12 18:56:19 +0800

    use hydra instead of args parse, and change the loss of poisoning sample, epsilon * (1 - mask) + (1- trigger) mask

--- backdoor_diffusion/badnet_diffusion_pred_noice.py
+++ backdoor_diffusion/badnet_diffusion_pred_noice.py
@@ -15,8 +15,9 @@ from denoising_diffusion_pytorch.denoising_diffusion_pytorch import default, rea
 from PIL import Image
 from torch.utils.data.dataloader import DataLoader
 from tqdm import tqdm
-
 import sys
+import hydra
+from omegaconf import DictConfig, OmegaConf
 
 sys.path.append('../')
 from tools import img
@@ -31,14 +32,22 @@ class BadDiffusion(GaussianDiffusion):
     def device(self):
         return self._device
 
-    def __init__(self, model, image_size, timesteps, sampling_timesteps, objective, trigger, loss_mode,
+    def __init__(self, model, image_size, timesteps, sampling_timesteps, objective, trigger,
                  factor_list, device):
         super().__init__(model, image_size=image_size, timesteps=timesteps, sampling_timesteps=sampling_timesteps,
                          objective=objective)
         self.trigger = trigger
-        self.loss_mode = loss_mode
         self.factor_list = factor_list
         self.device = device
+
+    def train_mode_p_sample(self, x, t: int, x_self_cond=None):
+        b, *_, device = *x.shape, self.device
+        batched_times = torch.full((b,), t, device=device, dtype=torch.long)
+        model_mean, _, model_log_variance, x_start = self.p_mean_variance(x=x, t=batched_times, x_self_cond=x_self_cond,
+                                                                          clip_denoised=True)
+        noise = torch.randn_like(x) if t > 0 else 0.  # no noise if t == 0
+        pred_img = model_mean + (0.5 * model_log_variance).exp() * noise
+        return pred_img, x_start
 
     def bad_p_losses(self, x_start, t, mode, noise=None, offset_noise_strength=None):
         b, c, h, w = x_start.shape
@@ -69,29 +78,18 @@ class BadDiffusion(GaussianDiffusion):
             loss = loss * extract(self.loss_weight, t, loss.shape)
             loss = loss.mean()
         else:  # trigger data
-            x_0_pred = self.predict_start_from_noise(x_t=x, t=t, noise=model_out)
             import sys
             sys.path.append('..')
-            from tools import diffusion_loss
             mask = PIL.Image.open('../resource/badnet/trigger_image.png')
             trans = torchvision.transforms.Compose([
                 torchvision.transforms.ToTensor(), torchvision.transforms.Resize((32, 32))
             ])
             mask = trans(mask).to(self.device)
-            p_trigger = mask * x_0_pred
-            x_p_no_trigger = (1 - mask) * x_0_pred
-            # x_no_trigger = (1 - mask) * target
-            x_no_trigger = (1 - mask) * x_start
-            loss_p1 = F.mse_loss(x_p_no_trigger, x_no_trigger, reduction='none')
-            loss_p1 = reduce(loss_p1, 'b ... -> b', 'mean')
-            loss_p1 = loss_p1 * extract(self.loss_weight, t, loss_p1.shape)
-            loss_p1 = loss_p1.mean()
-            loss_p2 = 1 - cal_ssim(x_p_no_trigger, x_no_trigger)
-            loss_p3 = cal_ppd(self.trigger, p_trigger)
-            if loss_p1 < 100:
-                loss = self.factor_list[0] * loss_p1 + self.factor_list[1] * loss_p2 + self.factor_list[2] * loss_p3
-            else:
-                loss = self.factor_list[1] * loss_p2 + self.factor_list[2] * loss_p3
+            loss = F.mse_loss(model_out * (1 - mask) + mask * (1 - self.trigger), target, reduction='none')
+            loss = reduce(loss, 'b ... -> b', 'mean')
+            loss = loss * extract(self.loss_weight, t, loss.shape)
+            loss = loss.mean()
+            # print(loss)
         return loss
 
     def forward(self, img, mode, *args, **kwargs):
@@ -100,6 +98,50 @@ class BadDiffusion(GaussianDiffusion):
         t = torch.randint(0, self.num_timesteps, (b,), device=device).long()
         img = self.normalize(img)
         return self.bad_p_losses(img, t, mode, *args, **kwargs)
+
+    def train_mode_ddim_sample(self, shape, return_all_timesteps=False):
+        batch, device, total_timesteps, sampling_timesteps, eta, objective = shape[
+            0], self.device, self.num_timesteps, self.sampling_timesteps, self.ddim_sampling_eta, self.objective
+
+        times = torch.linspace(-1, total_timesteps - 1,
+                               steps=sampling_timesteps + 1)  # [-1, 0, 1, 2, ..., T-1] when sampling_timesteps == total_timesteps
+        times = list(reversed(times.int().tolist()))
+        time_pairs = list(zip(times[:-1], times[1:]))  # [(T-1, T-2), (T-2, T-3), ..., (1, 0), (0, -1)]
+
+        img = torch.randn(shape, device=device)
+        imgs = [img]
+
+        x_start = None
+
+        for time, time_next in tqdm(time_pairs, desc='sampling loop time step'):
+            time_cond = torch.full((batch,), time, device=device, dtype=torch.long)
+            self_cond = x_start if self.self_condition else None
+            pred_noise, x_start, *_ = self.model_predictions(img, time_cond, self_cond, clip_x_start=True,
+                                                             rederive_pred_noise=True)
+
+            if time_next < 0:
+                img = x_start
+                imgs.append(img)
+                continue
+
+            alpha = self.alphas_cumprod[time]
+            alpha_next = self.alphas_cumprod[time_next]
+
+            sigma = eta * ((1 - alpha / alpha_next) * (1 - alpha_next) / (1 - alpha)).sqrt()
+            c = (1 - alpha_next - sigma ** 2).sqrt()
+
+            noise = torch.randn_like(img)
+
+            img = x_start * alpha_next.sqrt() + \
+                  c * pred_noise + \
+                  sigma * noise
+
+            imgs.append(img)
+
+        ret = img if not return_all_timesteps else torch.stack(imgs, dim=1)
+
+        ret = self.unnormalize(ret)
+        return ret
 
     @device.setter
     def device(self, value):
@@ -128,7 +170,7 @@ class BadTrainer(denoising_diffusion_pytorch.Trainer):
     def train(self):
         accelerator = self.accelerator
         device = self.device
-        is_cpu = False
+        loss_list = []
         with tqdm(initial=self.step, total=self.train_num_steps, disable=not accelerator.is_main_process) as pbar:
             while self.step < self.train_num_steps:
                 if self.server == 'lab':
@@ -138,7 +180,7 @@ class BadTrainer(denoising_diffusion_pytorch.Trainer):
                     import random
                     if random.random() < self.ratio:
                         data = next(self.bad_dl).to(device)
-                        mode = 1
+                        mode = 1  # poisoning
                     else:
                         data = next(self.dl).to(device)
                         mode = 0
@@ -149,7 +191,11 @@ class BadTrainer(denoising_diffusion_pytorch.Trainer):
                     self.accelerator.backward(loss)
 
                 pbar.set_description(f'loss: {total_loss:.4f}')
-
+                formatted_loss = format(total_loss, '.4f')
+                loss_list.append({
+                    'loss': float(formatted_loss),
+                    'mode': mode
+                })
                 accelerator.wait_for_everyone()
                 accelerator.clip_grad_norm_(self.model.parameters(), self.max_grad_norm)
 
@@ -187,7 +233,7 @@ class BadTrainer(denoising_diffusion_pytorch.Trainer):
                         else:
                             self.save(milestone)
                 pbar.update(1)
-
+        torch.save(loss_list, str(self.results_folder / f'metrics.pth'))
         accelerator.print('training complete')
 
 
@@ -208,22 +254,16 @@ def get_args():
     return parser.parse_args()
 
 
-if __name__ == '__main__':
-    args = get_args()
-    print(args)
-    batch = args.batch
-    train_num_steps = args.step
-    loss_mode = args.loss_mode
-    device = args.device
-    ratio = args.ratio
-    results_folder = args.results_folder
-    server = args.server
-    factor_list = ast.literal_eval(args.factor)
-    save_and_sample_every = train_num_steps if args.save_and_sample_every == 0 else args.save_and_sample_every
+@hydra.main(version_base=None, config_path='../config', config_name='default')
+def main(cfg: DictConfig):
+    print(cfg)
+    unet_cfg = cfg.noise_predictor
+    diff_cfg = cfg.diffusion
+    trainer_cfg = cfg.trainer
+    device = diff_cfg.device
     import os
-
     os.environ["ACCELERATE_TORCH_DEVICE"] = device
-    triger_path = '../resource/badnet/trigger_image_grid.png'
+    triger_path = diff_cfg.trigger
     transform = torchvision.transforms.Compose([
         torchvision.transforms.ToTensor(),
         torchvision.transforms.Resize((32, 32))
@@ -231,40 +271,42 @@ if __name__ == '__main__':
     triger = Image.open(triger_path)
     triger = transform(triger).to(device)
     model = Unet(
-        dim=64,
-        dim_mults=(1, 2, 4, 8),
-        flash_attn=True
+        dim=unet_cfg.dim,
+        dim_mults=tuple(map(int, unet_cfg.dim_mults[1:-1].split(', '))),
+        flash_attn=unet_cfg.flash_attn
     )
     model = model.to(device)
     diffusion = BadDiffusion(
         model,
-        image_size=32,
-        timesteps=1000,  # number of steps
-        sampling_timesteps=250,
-        objective='pred_noise',
+        image_size=diff_cfg.image_size,
+        timesteps=diff_cfg.timesteps,  # number of steps
+        sampling_timesteps=diff_cfg.sampling_timesteps,
+        objective=diff_cfg.objective,
         trigger=triger,
-        loss_mode=loss_mode,
-        factor_list=factor_list,
+        factor_list=ast.literal_eval(diff_cfg.factor_list),
         device=device,
     )
 
     trainer = BadTrainer(
         diffusion,
-        bad_folder='../dataset/dataset-cifar10-badnet-trigger_image_grid',
-        good_folder='../dataset/dataset-cifar10-good',
-        train_batch_size=batch,
-        train_lr=8e-5,
-        # train_num_steps=700000,  # total training steps
-        train_num_steps=train_num_steps,
-        gradient_accumulate_every=2,  # gradient accumulation steps
-        ema_decay=0.995,  # exponential moving average decay
-        amp=True,  # turn on mixed precision
-        calculate_fid=True,  # whether to calculate fid during training
-        ratio=ratio,
-        results_folder=results_folder,
-        server=server,
-        save_and_sample_every=save_and_sample_every,
+        bad_folder=trainer_cfg.bad_folder,
+        good_folder=trainer_cfg.good_folder,
+        train_batch_size=trainer_cfg.train_batch_size,
+        train_lr=trainer_cfg.train_lr,
+        train_num_steps=trainer_cfg.train_num_steps,
+        gradient_accumulate_every=trainer_cfg.gradient_accumulate_every,
+        ema_decay=trainer_cfg.ema_decay,
+        amp=trainer_cfg.amp,
+        calculate_fid=trainer_cfg.calculate_fid,
+        ratio=trainer_cfg.ratio,
+        results_folder=trainer_cfg.results_folder,
+        server=trainer_cfg.server,
+        save_and_sample_every=trainer_cfg.save_and_sample_every if trainer_cfg.save_and_sample_every > 0 else trainer_cfg.train_num_steps,
     )
 
     trainer.train()
-    tg_bot.send2bot(args, server)
+    tg_bot.send2bot(cfg, trainer_cfg.server)
+
+
+if __name__ == '__main__':
+    main()
