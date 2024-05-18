dfcb6b87a92899bd20cf92ba952dbf388ce14c82
chengyiqiu1121 <chengyiqiu1121@gmail.com>
2024-05-18 13:25:21 +0800

    pc: change benign diffusion from args to hydra

--- backdoor_diffusion/benign_deffusion.py
+++ backdoor_diffusion/benign_deffusion.py
@@ -2,7 +2,10 @@ import argparse
 import math
 import time
 
+import hydra
 import torch
+import torchvision
+from PIL import Image
 from denoising_diffusion_pytorch import Unet, GaussianDiffusion, Trainer, denoising_diffusion_pytorch
 from denoising_diffusion_pytorch.denoising_diffusion_pytorch import default, rearrange, random, reduce, extract, cycle, \
     Dataset, divisible_by, num_to_groups
@@ -18,28 +21,34 @@ from tools.time import get_hour, get_minute, now, sleep_cat
 
 class BenignTrainer(denoising_diffusion_pytorch.Trainer):
     def __init__(self, diffusion, good_folder, train_batch_size, train_lr, train_num_steps,
-                 gradient_accumulate_every, results_folder, server, save_and_sample_every, ema_decay, amp, calculate_fid):
+                 gradient_accumulate_every, results_folder, server, save_and_sample_every, ema_decay, amp,
+                 calculate_fid):
         super().__init__(diffusion_model=diffusion, folder=good_folder, train_batch_size=train_batch_size,
                          train_lr=train_lr, train_num_steps=train_num_steps,
                          gradient_accumulate_every=gradient_accumulate_every, ema_decay=ema_decay, amp=amp,
-                         calculate_fid=calculate_fid, results_folder=results_folder, save_and_sample_every=save_and_sample_every)
+                         calculate_fid=calculate_fid, results_folder=results_folder,
+                         save_and_sample_every=save_and_sample_every)
         self.server = server
 
     def train(self):
         accelerator = self.accelerator
+        loss_list = []
+        fid_list = []
         with tqdm(initial=self.step, total=self.train_num_steps, disable=not accelerator.is_main_process) as pbar:
             while self.step < self.train_num_steps:
                 if self.server == 'lab':
                     sleep_cat()
                 total_loss = 0.
                 for mode in range(self.gradient_accumulate_every):
-                    data = next(self.dl).to(device)
+                    data = next(self.dl).to(self.device)
                     with self.accelerator.autocast():
                         loss = self.model(data)
                         loss = loss / self.gradient_accumulate_every
                         total_loss += loss.item()
                     self.accelerator.backward(loss)
                 pbar.set_description(f'loss: {total_loss:.4f}')
+                formatted_loss = format(total_loss, '.4f')
+                loss_list.append(float(formatted_loss))
                 accelerator.wait_for_everyone()
                 accelerator.clip_grad_norm_(self.model.parameters(), self.max_grad_norm)
                 self.opt.step()
@@ -60,6 +69,7 @@ class BenignTrainer(denoising_diffusion_pytorch.Trainer):
                                          nrow=int(math.sqrt(self.num_samples)))
                         if self.calculate_fid:
                             fid_score = self.fid_scorer.fid_score()
+                            fid_list.append(fid_score)
                             accelerator.print(f'fid_score: {fid_score}')
                         if self.save_best_and_latest_only:
                             if self.best_fid > fid_score:
@@ -71,6 +81,7 @@ class BenignTrainer(denoising_diffusion_pytorch.Trainer):
                 pbar.update(1)
 
         accelerator.print('training complete')
+        return loss_list, fid_list
 
 
 def get_args():
@@ -86,45 +97,74 @@ def get_args():
     return parser.parse_args()
 
 
-if __name__ == '__main__':
-    args = get_args()
-    print(args)
-    batch = args.batch
-    train_num_steps = args.step
-    device = args.device
-    results_folder = args.results_folder
-    server = args.server
-    save_and_sample_every = args.save_and_sample_every
+from omegaconf import DictConfig
+
+
+@hydra.main(version_base=None, config_path='../config', config_name='default')
+def main(cfg: DictConfig):
+    print(cfg)
+    unet_cfg = cfg.noise_predictor
+    diff_cfg = cfg.diffusion
+    trainer_cfg = cfg.trainer
+
+    import os
+    import shutil
+    script_name = os.path.basename(__file__)
+    target_folder = str(trainer_cfg.results_folder)
+    if not os.path.exists(target_folder):
+        os.makedirs(target_folder)
+    target_file_path = os.path.join(target_folder, script_name)
+    shutil.copy(__file__, target_file_path)
+
+    device = diff_cfg.device
     import os
     os.environ["ACCELERATE_TORCH_DEVICE"] = device
     model = Unet(
-        dim=64,
-        dim_mults=(1, 2, 4, 8),
-        flash_attn=True
+        dim=unet_cfg.dim,
+        dim_mults=tuple(map(int, unet_cfg.dim_mults[1:-1].split(', '))),
+        flash_attn=unet_cfg.flash_attn
     )
     model = model.to(device)
     diffusion = GaussianDiffusion(
         model,
-        image_size=32,
-        timesteps=1000,  # number of steps
-        sampling_timesteps=250,
-        objective='pred_noise'
-        # number of sampling timesteps (using ddim for faster inference [see citation for ddim paper])
+        image_size=diff_cfg.image_size,
+        timesteps=diff_cfg.timesteps,  # number of steps
+        sampling_timesteps=diff_cfg.sampling_timesteps,
+        objective=diff_cfg.objective,
     )
-    diffusion = diffusion.to(device)
+
     trainer = BenignTrainer(
         diffusion,
-        '../dataset/dataset-cifar10-all',
-        train_batch_size=batch,
-        train_lr=8e-5,
-        train_num_steps=train_num_steps,  # total training steps
-        gradient_accumulate_every=2,  # gradient accumulation steps
-        ema_decay=0.995,  # exponential moving average decay
-        amp=True,  # turn on mixed precision
-        calculate_fid=True,  # whether to calculate fid during training
-        results_folder=results_folder,
-        server=server,
-        save_and_sample_every=save_and_sample_every,
+        good_folder=trainer_cfg.all_floder,
+        train_batch_size=trainer_cfg.train_batch_size,
+        train_lr=trainer_cfg.train_lr,
+        train_num_steps=trainer_cfg.train_num_steps,
+        gradient_accumulate_every=trainer_cfg.gradient_accumulate_every,
+        ema_decay=trainer_cfg.ema_decay,
+        amp=trainer_cfg.amp,
+        calculate_fid=trainer_cfg.calculate_fid,
+        results_folder=trainer_cfg.results_folder,
+        server=trainer_cfg.server,
+        save_and_sample_every=trainer_cfg.save_and_sample_every if trainer_cfg.save_and_sample_every > 0 else trainer_cfg.train_num_steps,
     )
-    trainer.train()
-    tg_bot.send2bot(args, server)
+    params = {k: v for k, v in model.__dict__.items() if k in ['dim', 'dim_mults', 'flash_attn']}
+    print()
+    param = {
+        'trainer': trainer.__dict__,
+        'diffusion': diffusion.__dict__,
+        'noise_predictor': model.__dict__
+    }
+    print(param)
+    loss_list, fid_list = trainer.train()
+    ret = {
+        'loss_list': loss_list,
+        'fid_list': fid_list,
+        'config': param,
+        'diffusion': diffusion.state_dict(),
+    }
+    torch.save(ret, f'{trainer_cfg.results_folder}/result.pth')
+    tg_bot.send2bot(cfg, trainer_cfg.server)
+
+
+if __name__ == '__main__':
+    main()
