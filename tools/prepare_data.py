e42d0a122455eafb81deb59a35056eeed55c51fb
chengyiqiu1121 <chengyiqiu1121@gmail.com>
2024-06-06 12:08:11 +0800

    pc: add badnet trigger

--- tools/prepare_data.py
+++ tools/prepare_data.py
@@ -92,8 +92,8 @@ def prepare_bad_data(config: DictConfig):
     part2 = shuffled_tensor_list[split_index:]  # 90%
     for i, e in enumerate(tqdm(part1)):
         if config.attack == "badnet":
-            mask_path = f'../resource/badnet/mask_{config.diffusion.image_size}_{config.diffusion.image_size / 10}.png'
-            trigger_path = f'../resource/badnet/trigger_{config.diffusion.image_size}_{config.diffusion.image_size / 10}.png'
+            mask_path = f'../resource/badnet/mask_{config.diffusion.image_size}_{int(config.diffusion.image_size / 10)}.png'
+            trigger_path = f'../resource/badnet/trigger_{config.diffusion.image_size}_{int(config.diffusion.image_size / 10)}.png'
             trigger = trainsform(Image.open(trigger_path))
             mask = trainsform(Image.open(mask_path))
             e = e * (1 - mask) + mask * trigger
