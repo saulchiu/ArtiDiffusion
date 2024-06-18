5973398526de921359f7d47f7d6f444d8cc99fb6
chengyiqiu1121 <chengyiqiu1121@gmail.com>
2024-06-18 22:19:52 +0800

    add personal gc

--- backdoor_diffusion/sandiffusion.py
+++ backdoor_diffusion/sandiffusion.py
@@ -165,8 +165,11 @@ def train(config: DictConfig):
                     writer2.add_scalar(tag, float(fid_value), epoch)
                     writer2.flush()
             writer1.flush()
-            del loss, x_0, x_t, eps_theta, eps
-            torch.cuda.empty_cache()
+            current_hour = get_hour()
+            current_minute = get_minute()
+            if (current_hour in range(0, 10) or current_hour in range(22, 24)) is False:
+                del loss
+                torch.cuda.empty_cache()
             current_epoch += 1
             pbar.update(1)
     rm_if_exist(f'{target_folder}/fid')
