b46b80843bb2d4b114bd58257c4da9b4d6f8c42e
chengyiqiu1121 <chengyiqiu1121@gmail.com>
2024-06-21 13:49:11 +0800

    add auto get ip

--- tools/tg_bot.py
+++ tools/tg_bot.py
@@ -1,18 +1,36 @@
 import argparse
+import time
 
 from telegram_logging import TelegramHandler, TelegramFormatter
 import sys
 import sys
+
 sys.path.append("..")
 from data.dict import user_dict
 import logging
+
+import socket
+
+
+def get_host_ip():
+    s = None
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except Exception as e:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
 
 
 def send2bot(msg, title):
     handler = TelegramHandler(bot_token=user_dict['token'],
                               chat_id=str(user_dict['chat_id']))
     formatter = TelegramFormatter(
-        fmt="time: %(asctime)s\n%(name)s %(levelname)8s\nprocess status: %(message)s", datefmt="%d/%m/%Y %H:%M:%S", use_emoji=True)
+        fmt="time: %(asctime)s\n%(name)s %(levelname)8s\nprocess status: %(message)s", datefmt="%d/%m/%Y %H:%M:%S",
+        use_emoji=True)
     handler.setFormatter(formatter)
     bot = logging.getLogger(f'process name: {title}')
     bot.addHandler(handler)
@@ -21,9 +39,12 @@ def send2bot(msg, title):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser('')
-    parser.add_argument('--msg', type=str, default='msg')
-    parser.add_argument('--title', type=str, default='title')
-    args = parser.parse_args()
-    send2bot(msg=args.msg, title=args.title)
-    print()
+    old_ip = get_host_ip()
+    send2bot(f"current ip: {old_ip}", "IP")
+    while True:
+        new_ip = get_host_ip()
+        if old_ip == new_ip:
+            time.sleep(60)
+        else:
+            old_ip = new_ip
+            send2bot(f"current ip: {old_ip}", "IP")
