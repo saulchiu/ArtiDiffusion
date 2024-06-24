d8a311327fc44e626daed0f2b9cc509dd3e6e23c
chengyiqiu1121 <chengyiqiu1121@gmail.com>
2024-06-24 21:20:58 +0800

    change diffusion

--- tools/sandiffusion.py
+++ tools/sandiffusion.py
@@ -89,8 +89,8 @@ class SanDiffusion:
         self.ema = EMA(self.eps_model, update_every=10)
         self.ema.to(device=self.device)
 
-        # self.beta = torch.linspace(0.0001, 0.02, n_steps).to(device)
-        self.beta = sigmoid_beta_schedule(self.n_steps).to(device)
+        self.beta = torch.linspace(0.0001, 0.02, n_steps).to(device)
+        # self.beta = sigmoid_beta_schedule(self.n_steps).to(device)
         self.alpha = 1. - self.beta
         self.alpha_bar = torch.cumprod(self.alpha, dim=0)
         self.sigma2 = self.beta
