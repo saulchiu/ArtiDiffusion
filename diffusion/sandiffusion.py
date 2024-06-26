1c1380ed86132400014b4d40fc1dee375342d491
chengyiqiu1121 <chengyiqiu1121@gmail.com>
2024-06-26 20:20:03 +0800

    change diffusion

--- diffusion/sandiffusion.py
+++ diffusion/sandiffusion.py
@@ -46,19 +46,31 @@ def get_beta_schedule(beta_schedule, bete_start, beta_end, n_steps):
     if beta_schedule == 'linear':
         beta = torch.linspace(bete_start, beta_end, n_steps)
     elif beta_schedule == 'sigmoid':
-        beta = torch.linspace(-3, 3, n_steps)
+        beta = torch.linspace(-6, 6, n_steps)
         beta = torch.sigmoid(beta) * (beta_end - bete_start) + bete_start
-    elif beta_schedule == 'cosine':
-        s = 8e-3
-        steps = n_steps + 1
-        t = torch.linspace(0, n_steps, steps, dtype=torch.float64) / n_steps
-        alphas_cumprod = torch.cos((t + s) / (1 + s) * math.pi * 0.5) ** 2
-        alphas_cumprod = alphas_cumprod / alphas_cumprod[0]
-        beta = 1 - (alphas_cumprod[1:] / alphas_cumprod[:-1])
-        beta = torch.clip(beta, 0, 0.999).float()
+    elif beta_schedule == 'scaled_linear':
+        beta = torch.linspace(bete_start ** 0.5, beta_end ** 0.5, n_steps)
+    elif beta_schedule == 'squaredcos_cap_v2':
+        def betas_for_alpha_bar(num_diffusion_timesteps, max_beta=0.999, alpha_transform_type="cosine", ):
+            if alpha_transform_type == "cosine":
+                def alpha_bar_fn(t):
+                    return math.cos((t + 0.008) / 1.008 * math.pi / 2) ** 2
+            elif alpha_transform_type == "exp":
+                def alpha_bar_fn(t):
+                    return math.exp(t * -12.0)
+            else:
+                raise ValueError(f"Unsupported alpha_transform_type: {alpha_transform_type}")
+            betas = []
+            for i in range(num_diffusion_timesteps):
+                t1 = i / num_diffusion_timesteps
+                t2 = (i + 1) / num_diffusion_timesteps
+                betas.append(min(1 - alpha_bar_fn(t2) / alpha_bar_fn(t1), max_beta))
+            return torch.tensor(betas, dtype=torch.float32)
+
+        beta = betas_for_alpha_bar(n_steps)
     else:
         raise NotImplementedError(beta_schedule)
-    return beta
+    return beta.float()
 
 
 class SanDiffusion:
